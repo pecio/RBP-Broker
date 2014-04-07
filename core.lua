90b65fd05f68f60b7f9315e2907930649d1c98a5
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-07 20:04:36 +0200

    Hopefully do actual notifications

--- core.lua
+++ core.lua
@@ -7,6 +7,7 @@ local AceConfigReg = LibStub("AceConfigRegistry-3.0")
 -- local L = LibStub("AceLocale-3.0"):GetLocate("LDBBroker")
 
 local UPDATEPERIOD, elapsed = 0.5, 0
+local RBPSPELL = 125439
 
 local dataojb = ldb:NewDataObject("Revive Battle Pets", { type = "data source", text = "Revive Battle Pets" })
 local f = CreateFrame("frame")
@@ -66,7 +67,7 @@ function RBPBroker:OnEnable()
   end
 
   -- Get and store Revive Battle Pets icon and name
-  local name, rank, icon, cost, isFunnel, powerType, castTime, minRange, maxRange = GetSpellInfo(125439)
+  local name, rank, icon, cost, isFunnel, powerType, castTime, minRange, maxRange = GetSpellInfo(RBPSPELL)
   PetHealthBroker.RBPicon = icon
   PetHealthBroker.RBPname = name
 
@@ -80,3 +81,37 @@ end
 function RBPBroker:OnInitialize()
   self.config = LibStub("AceDB-3.0"):New("RBPBrokerConfig", defaultOptions, true)
 end
+f:SetScript('OnUpdate', function(self, elap)
+  elapsed = elapsed + elap
+  if elapsed < UPDATEPERIOD then return end
+
+  elapsed = 0
+
+  local text = string.format('|T%s:16|t', RBPBroker.RBPicon)
+
+  if RBPBroker.config.profile.cooldown then
+    local start, duration, enabled = GetSpellCooldown(RBPSPELL)
+
+    local cooldown = start + duration - GetTime()
+
+    if cooldown > 0 then
+      RBPBroker.inCooldown = true
+      local min = math.floor(cooldown / 60)
+      local seg = cooldown % 60
+      text = text .. string.format(" |cFFFFFFFF%d:%02d|r", min, seg)
+    else
+      if RBPBroker.inCooldown then -- finished
+        RBPBroker.inCooldown = false
+
+        if RBPBroker.config.profile.notifyEnd == 'n1' or RBPBroker.config.profile.notifyEnd == 'n3' then -- sound or both
+          PlaySound("LEVELUP")
+        end
+        if RBPBroker.config.profile.notifyEnd == 'n2' or RBPBroker.config.profile.notifyEnd == 'n3' then -- chat or both
+          RBPBroker:Printf('%s is ready', string.format('|T%s:16|t %s', RBPBroker.RBPicon, RBPBroker.RBPname))
+        end
+      end
+
+      text = text .. string.format(" |cFF00FF0%s|r", "Ready")
+    end
+  end
+end)
