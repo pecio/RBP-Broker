a538d37ef4bb4c29aa3357658b04bdf595b99c54
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-07 22:00:02 +0200

    Add esES localization

---
+++ Locales/esES.lua
@@ -0,0 +1,29 @@
+local L = LibStub('AceLocale-3.0'):NewLocale('RBPBroker', 'esES')
+
+if not L then return end
+
+-- LibDataBroker data object title
+L['Revive Battle Pets'] = 'Revivir Mascotas'
+
+-- Configuration dialog
+L['Main'] = 'Principal'
+L['Show Cooldown'] = 'Mostrar tiempo de Reutilización'
+L['Show cooldown time for Revive Battle Pets spell in bar'] = 'Mostrar tiempo de Reutilización restante para la habilidad de Reivivir Mascotas en la barra'
+L['Notify Availability'] = 'Notificar Disponibilidad'
+L['Notify the player when cooldown time finishes'] = 'Notificar al jugador cuando el tiempode reutilización finalice'
+L['None'] = 'Ninguna'
+L['With Level Up Sound'] = 'Con sonido Subir de Nivel'
+L['In Chat'] = 'En el chat'
+L['Both'] = 'Ambas'
+
+-- Notifications
+-- Cooldown finished, RBP is ready; %s will be replaced by icon and name
+L['%s is ready'] = '%s está lista'
+
+
+-- Display
+-- Shortest possible way to say it is ready
+L['Ready'] = 'Lista'
+
+-- Tooltip
+L['Click for options'] = 'Click para opciones'
