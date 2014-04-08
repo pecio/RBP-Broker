254417acde58ba39831c8571526b4f418aa9838b
Raúl Pedroche <r.pedroche.novillo@gmail.com>
2014-04-08 23:40:48 +0200

    esES localization to battle start/end notifications

--- Locales/esES.lua
+++ Locales/esES.lua
@@ -15,11 +15,19 @@ L['None'] = 'Ninguna'
 L['With Level Up Sound'] = 'Con sonido Subir de Nivel'
 L['In Chat'] = 'En el chat'
 L['Both'] = 'Ambas'
+L['Report Current Status'] = 'Informar de Estado Actual'
+L['At Battle Start'] = 'Al Inicio de la Batalla'
+L['Notify current status in chat after battle start animation finishes'] = 'Notificar el estado actual en el chat después de que la animación de comienzo de la batalla termine'
+L['At Battle End'] = 'Al Final de la Batalla'
+L['Notify current status in chat on returning to normal game'] = 'Notificar el estado actual en el chat al volver al juego normal'
 
 -- Notifications
 -- Cooldown finished, RBP is ready; %s will be replaced by icon and name
 L['%s is ready'] = '%s está lista'
 
+-- Current status, not yet ready. First %s is RBP icon and name, second
+-- is time in long form (as per SecondsToTime API call)
+L['%s ready in %s'] = '%s lista en %s'
 
 -- Display
 -- Shortest possible way to say it is ready
